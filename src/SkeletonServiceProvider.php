2e489e0fa8bf03cf59ac5934e2f74074bea918e6
Le Van Quan <kiaisoft.quan.le@gmail.com>
2025-04-13 00:32:10 +0700

    Refactor commented code in SkeletonServiceProvider for clarity

--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -10,16 +10,16 @@ class SkeletonServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('skeleton')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_migration_table_name_table')
-            ->hasCommand(SkeletonCommand::class);
+        // /*
+        //  * This class is a Package Service Provider
+        //  *
+        //  * More info: https://github.com/spatie/laravel-package-tools
+        //  */
+        // $package
+        //     ->name('skeleton')
+        //     ->hasConfigFile()
+        //     ->hasViews()
+        //     ->hasMigration('create_migration_table_name_table')
+        //     ->hasCommand(SkeletonCommand::class);
     }
 }
